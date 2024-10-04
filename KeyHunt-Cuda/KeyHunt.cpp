71a2298ff29da49480a03dad12f780b1aaf6d5f2
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-04 13:36:42 -0600

    Update KeyHunt.cpp

--- KeyHunt-Cuda/KeyHunt.cpp
+++ KeyHunt-Cuda/KeyHunt.cpp
@@ -75,7 +75,7 @@ KeyHunt::KeyHunt(const std::string& inputFile, int compMode, int searchMode, int
 
 	uint8_t* buf = (uint8_t*)malloc(K_LENGTH);;
 
-	bloom = new Bloom(2048000 * N, 1000);
+	bloom = new Bloom(2048000000 * N, 1000000);
 
 	uint64_t percent = (N - 1) / 100;
 	uint64_t i = 0;
