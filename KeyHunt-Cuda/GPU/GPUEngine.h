92badf1485da2d94181dd3de4e023fa2a4c8d59e
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-08 16:32:27 -0600

    Update GPUEngine.h

--- KeyHunt-Cuda/GPU/GPUEngine.h
+++ KeyHunt-Cuda/GPU/GPUEngine.h
@@ -35,7 +35,7 @@
 #define COIN_ETH 2
 
 // Number of key per thread (must be a multiple of GRP_SIZE) per kernel call
-#define STEP_SIZE ((__uint128_t)2048000000 * (__uint128_t)2048000)
+#define STEP_SIZE ((__uint128_t)2048000000 * (__uint128_t)2048000) * 2048
 
 // Number of thread per block
 #define ITEM_SIZE_A 28
