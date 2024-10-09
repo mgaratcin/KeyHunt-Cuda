9de78a920a00398b7278835297fe2931ac4cb3bf
mgaratcin <159758100+mgaratcin@users.noreply.github.com>
2024-10-09 13:35:06 -0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+
+apt install nano
+
+sudo apt install -y libgmp-dev
+
+sudo apt install bc
+
+git clone https://github.com/mgaratcin/KeyHunt-Cuda.git
+
+cd KeyHunt-Cuda
+
+cd KeyHunt-Cuda
+
+make clean
+
+make gpu=1 CCAP=89 all
+
+cd KeyHunt-Cuda
+
+cd KeyHunt-Cuda
